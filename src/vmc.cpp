3ecc92ebf0c771175587c03713afcc9e30033011
augude <augustgude@gmail.com>
2023-01-25 14:47:39 +0100

    Finding minima for 1 particle in 1D

--- src/vmc.cpp
+++ src/vmc.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 
 double local_energy(double position, double alpha){
-    double energy = alpha + position*position*(0.5 - 2*alpha*alpha);
+    //double energy = alpha + position*position*(0.5 - 2*alpha*alpha);
+    double energy = position*position*(0.5-2*alpha*alpha) + alpha;
     return energy;
 }
 
@@ -21,28 +22,29 @@ void monte_carlo(int MC_cycles, double step){
     mt19937 generator;
     generator.seed(seed);
     uniform_real_distribution<double> rng_double(0,1);
+    double position = 0.0;
 
     vec alpha_values = arma::linspace(0.1, 1.0, 10);
-    vec energy_mean = vec(alpha_values.size());
-    vec energy_variance = vec(alpha_values.size());
+    //vec energy_mean = vec(alpha_values.size());
+    //vec energy_variance = vec(alpha_values.size());
     for (int i = 0; i < alpha_values.size(); i++){
         double alpha = alpha_values[i];
         double energy = 0.0;
         double energy_squared = 0.0;
-        double position = 0.0;
         for (int j = 0; j < MC_cycles; j++){
-            double new_position = position + step*rng_double(generator);
+            double new_position = position + step*(-rng_double(generator) + 0.5);
             double ratio = probability_ratio(position, new_position, alpha);
-            if (rng_double(generator) < ratio){
+            if (rng_double(generator) <= ratio){
                 position = new_position;
             }
             double new_energy = local_energy(position, alpha);
             energy += new_energy;
             energy_squared += new_energy*new_energy;
         }
-        energy_mean[i] = energy/MC_cycles;
-        energy_variance[i] = (energy_squared - energy*energy/MC_cycles)/(MC_cycles - 1);
-        cout << "alpha = " << alpha_values[i] << " energy = " << energy_mean[i] << " variance = " << energy_variance[i] << endl;
+        energy = energy/MC_cycles;
+        double energy2 = energy_squared/MC_cycles;
+        double energy_variance = energy2 - energy*energy;
+        cout << "alpha = " << alpha_values[i] << " energy = " << energy << " variance = " << energy_variance << endl;
     }
 
 }
